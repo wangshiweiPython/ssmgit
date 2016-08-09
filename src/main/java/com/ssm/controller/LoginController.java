c0838a26c9d0ec502aaefabeea37a076ecbb1285
wsw <1183963655@qq.com>
2016-08-09 16:37:03 +0800

    20160809

---
+++ src/main/java/com/ssm/controller/LoginController.java
@@ -0,0 +1,7 @@
+package com.ssm.controller;
+
+/**
+ * Created by wangshiwei on 2016/8/9.
+ */
+public class LoginController {
+}
---
+++ src/main/java/com/ssm/service/LoginService.java
@@ -0,0 +1,7 @@
+package com.ssm.service;
+
+/**
+ * Created by wangshiwei on 2016/8/9.
+ */
+public class LoginService {
+}
